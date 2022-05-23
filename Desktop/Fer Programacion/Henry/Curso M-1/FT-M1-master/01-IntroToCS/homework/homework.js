d370f9ba30aca66d1b2a64a206eb8b331b67fdb7
unknown <fdiaz4823@gmail.com>
2022-05-23 16:17:44 -0300

    Homework 01 finalizada

--- Desktop/Fer Programacion/Henry/Curso M-1/FT-M1-master/01-IntroToCS/homework/homework.js
+++ Desktop/Fer Programacion/Henry/Curso M-1/FT-M1-master/01-IntroToCS/homework/homework.js
@@ -3,11 +3,25 @@
 function BinarioADecimal(num) {
   // tu codigo aca
 
+
+  let tam = num.length;
+  let sum = 0;
+  for (let i = 0; i < num.length; i++) {
+      sum = sum + num[i] * 2 ** (tam - 1 - i);
+  }
+  return sum;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  let bin = "";
+  while (num > 0) {
+      let resto = num % 2;
+      bin = resto + bin;
+      num = Math.floor(num / 2);
 
+  }
+  return bin;
 }
 
 
